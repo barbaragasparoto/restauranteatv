eb7ed1672d44efdb6a3a1bbeee669c7a2c8f593e
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-09-25 10:58:20 -0300

    25/09

--- src/Pages/Toolbar/index.tsx
+++ src/Pages/Toolbar/index.tsx
@@ -1,12 +1,15 @@
-
+import styles from './Toolbar.module.scss';
 import topo from "../../assets/topo.png"
 
 export default function Toolbar () {
     return(
-
-         <nav >
-        <img src={ topo } alt="Logo do Restaurante"/>
-        </nav>
+      <main> 
+          
+        <img className={styles.topo} src={ topo } alt="Imagem de topo"/>
+        <h3 className={styles.titulo}>Cardápio</h3>
+        
+      </main>
+        
     
       );
     }
