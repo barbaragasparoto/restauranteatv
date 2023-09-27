62fe85f3e5e88d07521c06ccaeda121eef3f5688
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-09-27 08:58:37 -0300

    27/09

--- src/Pages/Buscador/index.tsx
+++ src/Pages/Buscador/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Buscador.module.scss';
+import styles from './buscador.module.scss';
 import {CgSearch} from 'react-icons/cg';
 
 
@@ -9,7 +9,9 @@ interface Props {
 
 export default function Buscador({busca, setBusca}: Props) {
     return (
+        
         <div className={styles.buscador}>
+            
             <input
             value={busca}
             onChange={(evento) => setBusca(evento.target.value)}
--- src/Pages/Catálogo/index.tsx
+++ src/Pages/Catálogo/index.tsx
@@ -1,9 +1,23 @@
 import styles from './Catalogo.module.scss';
 import Buscador from '../Buscador';
+import { useState } from 'react';
+import Filtros from '../Filtros';
 
 export default function Catalogo () {
+
+  const [busca, setBusca] = useState("");
+
+
+
 return(
 
-null
-  );
+<main>
+
+<Buscador busca={busca} setBusca={setBusca}/>
+<div className={styles.catalogo__filtros}>
+<Filtros/>
+</div>  
+</main>
+);
+
 }
